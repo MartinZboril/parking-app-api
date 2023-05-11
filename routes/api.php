a45f6cbe1f67f49b3ec995f66b2ba807ae5a7c14
MartinZboril <martinzboril1@hotmail.com>
2023-05-11 17:39:54 +0200

    create routes for displaying zones

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\Api\V1\Auth;
+use App\Http\Controllers\Api\V1\ZoneController;
 use App\Http\Controllers\Api\V1\VehicleController;
 
 /*
@@ -22,6 +23,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('auth/register', Auth\RegisterController::class);
 Route::post('auth/login', Auth\LoginController::class);
+
+Route::get('zones', [ZoneController::class, 'index']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('profile', [Auth\ProfileController::class, 'show']);
